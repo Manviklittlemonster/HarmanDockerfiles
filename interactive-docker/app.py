75c7cd3376db4881bec00690576af9f8cf6d9832
Harman <Harman@123.com>
2026-06-19 06:20:59 +0530

    my Commit

---
+++ interactive-docker/app.py
@@ -0,0 +1,7 @@
+print('Program to sum two numbers')
+num1 = int(input('enter first no--'))
+num2 = int(input('enter second no--'))
+
+result = num1 + num2
+
+print(f'sum of two numbers are {result}')
